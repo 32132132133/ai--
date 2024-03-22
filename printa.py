68d2450d34a4d783d0dd6b9b37cdd36617260513
32132132133 <163497658+32132132133@users.noreply.github.com>
2024-03-22 10:40:01 +0900

    Add files via upload

---
+++ printa.py
@@ -0,0 +1,5 @@
+# Get input from the user
+user_input = input("Enter something: ")
+
+# Print the user's input
+print("You entered:", user_input)
---
+++ printab.py
@@ -0,0 +1,23 @@
+# Get the first input from the user
+a = float(input("Enter the first number: "))
+
+# Get the second input from the user
+b = float(input("Enter the second number: "))
+
+# Perform addition
+sum = a + b
+
+# Perform subtraction
+difference = a - b
+
+# Perform multiplication
+product = a * b
+
+# Perform division
+quotient = a / b
+
+# Print the results
+print("The sum is:", sum)
+print("The difference is:", difference)
+print("The product is:", product)
+print("The quotient is:", quotient)
